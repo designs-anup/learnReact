75786cf60d72f61566866c02f52566d6fe910e7b
Anup Kumar Sahoo <designs.anup@gmail.com>
2026-08-24 20:17:02 +0000

    basis usestate hook

--- revise/01basic/src/Chai.jsx
+++ revise/01basic/src/Chai.jsx
@@ -1,6 +1,23 @@
+import {useState} from 'react'
+
 function Chai(){
+    let [counter, setCounter] = useState(5);
+    //let counter = 10;
+
+    const addValue = () =>{
+        //console.log("Value added")
+        setCounter(counter += 1);
+    }
+
+    const removeValue = () => {
+        setCounter(counter -= 1);
+    }
     return(
-        <h2>Learn with Chai aur code</h2>
+        <>
+            <h2>Total Chai Cups {counter}</h2>
+            <button onClick={addValue}>Increase Cup</button>
+            <button onClick={removeValue}>Decrease Cup</button>
+        </>
     );
 }
 
